9349be5d8316895cfb16649c8ee2657f42efcada
Aman Sunesh <157351006+Aman-Sunesh@users.noreply.github.com>
2025-01-18 11:23:43 +0000

    Initial commit of all project files

--- script.js
+++ script.js
@@ -1,8 +1,37 @@
-/* HAMBURGER NAV MENU TOGGLE (Mobile) */
-const hamburger = document.getElementById("hamburger");
-const navLinks = document.getElementById("navLinks");
+// Show or hide the pop-up
+function showPopup(show) 
+{
+    const popup = document.getElementById("popup");
+    if (show) 
+    {
+      popup.style.visibility = "visible";
+    } 
+    
+    else 
+    {
+      popup.style.visibility = "hidden";
+    }
+}
 
-hamburger.addEventListener("click", () => {
-  navLinks.classList.toggle("open");
-});
+// Add reccommendation
+function addRecommendation() 
+{
+  let recommendation = document.getElementById("new_recommendation");
 
+  if (recommendation.value.trim() !== "") 
+    {
+    const newDiv = document.createElement("div");
+    newDiv.classList.add("recommendation");
+    newDiv.innerHTML = `<span>&#8220;</span>${recommendation.value}<span>&#8221;</span>`;
+    document.getElementById("all_recommendations").appendChild(newDiv);
+
+    recommendation.value = "";
+    // Use the custom pop-up instead of alert
+    showPopup(true);
+  } 
+  
+  else 
+  {
+    alert("Please write a recommendation before submitting.");
+  }
+}
