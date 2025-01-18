8305ba315e3f80e816075e30c6cfca14485d8138
Aman Sunesh <157351006+Aman-Sunesh@users.noreply.github.com>
2025-01-18 13:01:29 +0000

    Integrate EmailJS into Contact Me section

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 // Show or hide the pop-up
+// Function to show or hide the pop-up
 function showPopup(show) 
 {
     const popup = document.getElementById("popup");
@@ -35,3 +36,29 @@ function addRecommendation()
     alert("Please write a recommendation before submitting.");
   }
 }
+
+
+// Handle Contact Form Submission
+document.getElementById('contact-form').addEventListener('submit', function(event) 
+{
+  event.preventDefault(); // Prevent the default form submission
+
+  // Send the email using EmailJS
+  emailjs.send("service_at13aoc", "template_z39a68s", 
+    {
+    user_name: event.target.user_name.value,
+    user_email: event.target.user_email.value,
+    user_message: event.target.user_message.value
+  })
+  .then(function(response) 
+  {
+     console.log('SUCCESS!', response.status, response.text);
+     showPopup(true); // Show success popup
+     event.target.reset(); // Reset the form fields
+  }, function(error) 
+  {
+     console.log('FAILED...', error);
+     alert('There was an error sending your message. Please try again later.');
+  });
+  
+});
